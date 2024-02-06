1b271de764f0898cdd82fb38d06383a31234393e
laksix <147379934+laksix@users.noreply.github.com>
2024-02-07 01:41:40 +0700

    Update footer.jsx

--- todo/src/components/Footer/footer.jsx
+++ todo/src/components/Footer/footer.jsx
@@ -1,1 +1,27 @@
-import React, { Component } from 'react';import FooterList from '../Footer-list';import './footer.css';export default class Footer extends Component {  countItem = (elements) => {    let count = 0;    elements.forEach((e) => {      if (!e.hidden) {        count++;      }    });    return `${count} item left`;  };  render() {    const { clearCompleted } = this.props;    return (      <footer className="footer">        <span className="todo-count">{this.countItem(this.props.elements)}</span>        <FooterList footerItems={this.props.footerItems} el={this.props.elements} />        <button onClick={() => clearCompleted()} className="clear-completed">          Clear completed        </button>      </footer>    );  }}
+import React, { Component } from 'react';
+import FooterList from '../Footer-list';
+import './footer.css';
+export default class Footer extends Component {
+  countItem = (elements) => {
+    let count = 0;
+    elements.forEach((e) => {
+      if (!e.hidden) {
+        count++;
+      }
+    });
+    return `${count} item left`;
+  };
+
+  render() {
+    const { clearCompleted } = this.props;
+    return (
+      <footer className="footer">
+        <span className="todo-count">{this.countItem(this.props.elements)}</span>
+        <FooterList footerItems={this.props.footerItems} el={this.props.elements} />
+        <button onClick={() => clearCompleted()} className="clear-completed">
+          Clear completed
+        </button>
+      </footer>
+    );
+  }
+}

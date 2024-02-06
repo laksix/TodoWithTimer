40e1276d2368746b35165ddc44886b1f82e18cd9
laksix <147379934+laksix@users.noreply.github.com>
2024-02-07 01:42:51 +0700

    Update todo-list.jsx

--- todo/src/components/Todo-list/todo-list.jsx
+++ todo/src/components/Todo-list/todo-list.jsx
@@ -1,1 +1,37 @@
-import React, { Component } from 'react';import TodoIistItem from '../Todo-list-item';import './todo-list.css';export default class Todolist extends Component {  render() {    const { onDeleted, onItemClick, onStartEdit, onFinishEdit } = this.props;    return (      <ul className="todo-list">        {this.props.elements.map((e) => {          return (            <TodoIistItem              onFinishEdit={(event) => {                if (event.key === 'Enter') {                  onFinishEdit(e.id);                }              }}              setNameEdit={this.props.setNameEdit}              id={e.id}              editing={e.editing}              onStartEdit={(event) => {                event.stopPropagation();                onStartEdit(e.id);              }}              name={e.name}              key={e.id}              onItemClick={() => onItemClick(e.id)}              completed={e.completed}              hidden={e.hidden}              onDeleted={() => onDeleted(e.id)}            />          );        })}      </ul>    );  }}
+import React, { Component } from 'react';
+import TodoIistItem from '../Todo-list-item';
+import './todo-list.css';
+
+export default class Todolist extends Component {
+  render() {
+    const { onDeleted, onItemClick, onStartEdit, onFinishEdit } = this.props;
+    return (
+      <ul className="todo-list">
+        {this.props.elements.map((e) => {
+          return (
+            <TodoIistItem
+              onFinishEdit={(event) => {
+                if (event.key === 'Enter') {
+                  onFinishEdit(e.id);
+                }
+              }}
+              setNameEdit={this.props.setNameEdit}
+              id={e.id}
+              editing={e.editing}
+              onStartEdit={(event) => {
+                event.stopPropagation();
+                onStartEdit(e.id);
+              }}
+              name={e.name}
+              key={e.id}
+              onItemClick={() => onItemClick(e.id)}
+              completed={e.completed}
+              hidden={e.hidden}
+              onDeleted={() => onDeleted(e.id)}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+}

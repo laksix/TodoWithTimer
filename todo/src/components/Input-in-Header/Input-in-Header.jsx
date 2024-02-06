e7d7d028cc8aeb98efdfed60c72d9b2843b6adac
laksix <147379934+laksix@users.noreply.github.com>
2024-02-07 01:42:18 +0700

    Update Input-in-Header.jsx

--- todo/src/components/Input-in-Header/Input-in-Header.jsx
+++ todo/src/components/Input-in-Header/Input-in-Header.jsx
@@ -1,1 +1,16 @@
-import React, { Component } from 'react';import './Input-in-Header.css';export default class Input extends Component {  render() {    return (      <input        className="new-todo"        value={this.props.name}        placeholder="What needs to be done?"        autoFocus        onChange={this.props.addNewText}        onKeyDown={this.props.setCurrentName}      ></input>    );  }}
+import React, { Component } from 'react';
+import './Input-in-Header.css';
+export default class Input extends Component {
+  render() {
+    return (
+      <input
+        className="new-todo"
+        value={this.props.name}
+        placeholder="What needs to be done?"
+        autoFocus
+        onChange={this.props.addNewText}
+        onKeyDown={this.props.setCurrentName}
+      ></input>
+    );
+  }
+}

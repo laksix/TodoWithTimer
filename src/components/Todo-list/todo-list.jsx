ed6cc6989e87b429965219c655452f0b04c44462
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:31:44 +0700

    Update todo-list.jsx

--- src/components/Todo-list/todo-list.jsx
+++ src/components/Todo-list/todo-list.jsx
@@ -1,1 +1,38 @@
-import React from 'react';import TodoIistItem from '../Todo-list-item';import './todo-list.css';const TodoList = ({ elements, onDeleted, onItemClick, onStartEdit, onFinishEdit, setNameEdit, min, sec }) => {  const currentMin = Number(min);  const currentSec = Number(sec);  const time = currentMin * 60 + currentSec;  return (    <ul className="todo-list">      {elements.map((e) => (        <TodoIistItem          time={time}          onFinishEdit={(event) => {            if (event.key === 'Enter') {              onFinishEdit(e.id);            }          }}          setNameEdit={setNameEdit}          id={e.id}          editing={e.editing}          onStartEdit={(event) => {            event.stopPropagation();            onStartEdit(e.id);          }}          name={e.name}          key={e.id}          onItemClick={() => onItemClick(e.id)}          completed={e.completed}          hidden={e.hidden}          onDeleted={() => onDeleted(e.id)}        />      ))}    </ul>  );};export default TodoList;
+import React from 'react';
+import TodoIistItem from '../Todo-list-item';
+import './todo-list.css';
+
+const TodoList = ({ elements, onDeleted, onItemClick, onStartEdit, onFinishEdit, setNameEdit, min, sec }) => {
+  const currentMin = Number(min);
+  const currentSec = Number(sec);
+  const time = currentMin * 60 + currentSec;
+
+  return (
+    <ul className="todo-list">
+      {elements.map((e) => (
+        <TodoIistItem
+          time={time}
+          onFinishEdit={(event) => {
+            if (event.key === 'Enter') {
+              onFinishEdit(e.id);
+            }
+          }}
+          setNameEdit={setNameEdit}
+          id={e.id}
+          editing={e.editing}
+          onStartEdit={(event) => {
+            event.stopPropagation();
+            onStartEdit(e.id);
+          }}
+          name={e.name}
+          key={e.id}
+          onItemClick={() => onItemClick(e.id)}
+          completed={e.completed}
+          hidden={e.hidden}
+          onDeleted={() => onDeleted(e.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+export default TodoList;

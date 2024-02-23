a4ed6ab2b4fabda17fa42bf747dcc64ac7a5bdf2
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:30:52 +0700

    Update footer.jsx

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,1 +1,26 @@
-import React from 'react';import FooterList from '../Footer-list';import './footer.css';const Footer = ({ elements, footerItems, clearCompleted }) => {  const countItem = (elements) => {    let count = 0;    elements.forEach((e) => {      if (!e.hidden) {        count++;      }    });    return `${count} item left`;  };  return (    <footer className="footer">      <span className="todo-count">{countItem(elements)}</span>      <FooterList footerItems={footerItems} el={elements} />      <button onClick={clearCompleted} className="clear-completed">        Clear completed      </button>    </footer>  );};export default Footer;
+import React from 'react';
+import FooterList from '../Footer-list';
+import './footer.css';
+const Footer = ({ elements, footerItems, clearCompleted }) => {
+  const countItem = (elements) => {
+    let count = 0;
+    elements.forEach((e) => {
+      if (!e.hidden) {
+        count++;
+      }
+    });
+    return `${count} item left`;
+  };
+
+  return (
+    <footer className="footer">
+      <span className="todo-count">{countItem(elements)}</span>
+      <FooterList footerItems={footerItems} el={elements} />
+      <button onClick={clearCompleted} className="clear-completed">
+        Clear completed
+      </button>
+    </footer>
+  );
+};
+
+export default Footer;

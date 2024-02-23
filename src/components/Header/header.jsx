42d2da72728960642e75945a293ae17746ff7ca3
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:31:03 +0700

    Update header.jsx

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,1 +1,19 @@
-import React from 'react';import Input from '../Input-in-Header';import './header.css';const Header = ({ clearInput, setTimeSec, setTimeMin, addNewText, setCurrentName }) => {  return (    <header className="header">      <h1>Todos</h1>      <Input        clearInput={clearInput}        setTimeSec={setTimeSec}        setTimeMin={setTimeMin}        addNewText={addNewText}        setCurrentName={setCurrentName}      />    </header>  );};export default Header;
+import React from 'react';
+import Input from '../Input-in-Header';
+import './header.css';
+
+const Header = ({ clearInput, setTimeSec, setTimeMin, addNewText, setCurrentName }) => {
+  return (
+    <header className="header">
+      <h1>Todos</h1>
+      <Input
+        clearInput={clearInput}
+        setTimeSec={setTimeSec}
+        setTimeMin={setTimeMin}
+        addNewText={addNewText}
+        setCurrentName={setCurrentName}
+      />
+    </header>
+  );
+};
+export default Header;

fa2bf55e0c451b4fcfe40158593d9678797ce6e9
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:30:09 +0700

    Update app.jsx

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,1 +1,8 @@
-import React from 'react';import FirstSection from '../First-section/fitst-section';const App = () => {  return <FirstSection />;};export default App;
+import React from 'react';
+import FirstSection from '../First-section/fitst-section';
+
+const App = () => {
+  return <FirstSection />;
+};
+
+export default App;

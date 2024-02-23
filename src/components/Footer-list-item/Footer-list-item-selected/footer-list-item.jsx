b499252eedf4a600a70bd6b7a15dd7ed216db0a6
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:30:31 +0700

    Update footer-list-item.jsx

--- src/components/Footer-list-item/Footer-list-item-selected/footer-list-item.jsx
+++ src/components/Footer-list-item/Footer-list-item-selected/footer-list-item.jsx
@@ -1,1 +1,17 @@
-import React from 'react';import './footer-list-item.css';const FooterItem = ({ status, name, click, id }) => {  return (    <li>      <button        className={status ? 'selected' : ''}        onClick={() => {          click(id);        }}      >        {name}      </button>    </li>  );};export default FooterItem;
+import React from 'react';
+import './footer-list-item.css';
+const FooterItem = ({ status, name, click, id }) => {
+  return (
+    <li>
+      <button
+        className={status ? 'selected' : ''}
+        onClick={() => {
+          click(id);
+        }}
+      >
+        {name}
+      </button>
+    </li>
+  );
+};
+export default FooterItem;

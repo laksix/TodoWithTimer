02433ed72339ab8e269cb08cb212bcc02ab5cd56
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:30:41 +0700

    Update footer-list.jsx

--- src/components/Footer-list/footer-list.jsx
+++ src/components/Footer-list/footer-list.jsx
@@ -1,1 +1,15 @@
-import React from 'react';import FooterItem from '../Footer-list-item/Footer-list-item-selected';import './footer-list.css';const FooterList = ({ footerItems }) => {  return (    <ul className="filters">      {footerItems.map((e) => {        return <FooterItem name={e.name} key={e.id} id={e.id} status={e.selected} click={e.onClick} />;      })}    </ul>  );};export default FooterList;
+import React from 'react';
+import FooterItem from '../Footer-list-item/Footer-list-item-selected';
+import './footer-list.css';
+
+const FooterList = ({ footerItems }) => {
+  return (
+    <ul className="filters">
+      {footerItems.map((e) => {
+        return <FooterItem name={e.name} key={e.id} id={e.id} status={e.selected} click={e.onClick} />;
+      })}
+    </ul>
+  );
+};
+
+export default FooterList;

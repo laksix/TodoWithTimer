470bb023f52074a0af553601dc885a7b36162d39
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:32:00 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,8 @@
-import React from 'react';import { createRoot } from 'react-dom/client';import './index.css';import App from './components/App/app';const container = document.getElementById('app');const root = createRoot(container);root.render(<App />);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App/app';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);

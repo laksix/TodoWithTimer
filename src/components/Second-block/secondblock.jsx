ffcfb89468d8e69d9b4bcd68a66462023ef89eba
laksix <147379934+laksix@users.noreply.github.com>
2024-02-23 20:31:21 +0700

    Update secondblock.jsx

--- src/components/Second-block/secondblock.jsx
+++ src/components/Second-block/secondblock.jsx
@@ -1,1 +1,33 @@
-import React from 'react';import Todolist from '../Todo-list/todo-list';import Footer from '../Footer/footer';import './secondblock.css';const SecondBlock = ({  min,  sec,  onFinishEdit,  setNameEdit,  onStartEdit,  onDeleted,  onItemClick,  elements,  clearCompleted,  footerItems,}) => {  return (    <section className="main">      <Todolist        min={min}        sec={sec}        onFinishEdit={onFinishEdit}        setNameEdit={setNameEdit}        onStartEdit={onStartEdit}        onDeleted={onDeleted}        onItemClick={onItemClick}        elements={elements}      />      <Footer clearCompleted={clearCompleted} footerItems={footerItems} elements={elements} />    </section>  );};export default SecondBlock;
+import React from 'react';
+import Todolist from '../Todo-list/todo-list';
+import Footer from '../Footer/footer';
+import './secondblock.css';
+const SecondBlock = ({
+  min,
+  sec,
+  onFinishEdit,
+  setNameEdit,
+  onStartEdit,
+  onDeleted,
+  onItemClick,
+  elements,
+  clearCompleted,
+  footerItems,
+}) => {
+  return (
+    <section className="main">
+      <Todolist
+        min={min}
+        sec={sec}
+        onFinishEdit={onFinishEdit}
+        setNameEdit={setNameEdit}
+        onStartEdit={onStartEdit}
+        onDeleted={onDeleted}
+        onItemClick={onItemClick}
+        elements={elements}
+      />
+      <Footer clearCompleted={clearCompleted} footerItems={footerItems} elements={elements} />
+    </section>
+  );
+};
+export default SecondBlock;
